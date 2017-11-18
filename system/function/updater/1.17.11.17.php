14ba80dbef7db6635886885c207a06722e947c4e
tsgirl <tsgirl@users.noreply.github.com>
2017-11-18 21:24:45 +0800

    修正乱码

--- system/function/updater/1.17.11.17.php
+++ system/function/updater/1.17.11.17.php
@@ -2,5 +2,5 @@
   if(!defined('IN_KKFRAME')) exit('Access Denied');
   DB::query("ALTER TABLE `member_setting` ADD `sign_method` tinyint(1) NOT NULL DEFAULT '3';");
   saveSetting('version', '1.17.11.18');
-  showmessage('³É¹¦¸üÐÂµ½ 1.17.11.18£¡', './');
+  showmessage('æˆåŠŸæ›´æ–°åˆ° 1.17.11.18ï¼', './');
 ?>
